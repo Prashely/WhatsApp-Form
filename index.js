a9276faad2ea52ebe6348018b67c695c9bc52810
Prashely <danielprashely@gmail.com>
2024-06-24 17:24:35 +0200

    Second Commit - Rearranging Code

---
+++ index.js
@@ -0,0 +1,10 @@
+function sendToWhatsApp(){
+
+    const inputEmail = document.getElementById("email").value;
+    const  inputName = document.getElementById("fname").value;
+
+    const formUrl = "https://wa.me/+27734737725" +  "?text=" 
+    +"Hey, I hope you are well, my name "+encodeURIComponent(inputName)+ " and my email is "+encodeURIComponent(inputEmail)+ ". I would like to book you for an Event. ";
+    window.open(formUrl, '_blank').focus();
+
+} 
