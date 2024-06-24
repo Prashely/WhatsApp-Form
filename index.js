4086edc6d81cb25ba63f433cac43b5aa2d740352
Prashely <danielprashely@gmail.com>
2024-06-24 17:31:08 +0200

    Chnaged Phone number

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function sendToWhatsApp(){
     const inputEmail = document.getElementById("email").value;
     const  inputName = document.getElementById("fname").value;
 
-    const formUrl = "https://wa.me/+27734737725" +  "?text=" 
+    const formUrl = "https://wa.me/+27683928066" +  "?text=" 
     +"Hey, I hope you are well, my name "+encodeURIComponent(inputName)+ " and my email is "+encodeURIComponent(inputEmail)+ ". I would like to book you for an Event. ";
     window.open(formUrl, '_blank').focus();
 
