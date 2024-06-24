4150c65d6ede5a4cc3595cbf7d0c54d216c7f8ad
Prashely <danielprashely@gmail.com>
2024-06-24 17:31:49 +0200

    Updated Submit button and Message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function sendToWhatsApp(){
     const  inputName = document.getElementById("fname").value;
 
     const formUrl = "https://wa.me/+27683928066" +  "?text=" 
-    +"Hey, I hope you are well, my name "+encodeURIComponent(inputName)+ " and my email is "+encodeURIComponent(inputEmail)+ ". I would like to book you for an Event. ";
+    +"Hey, I hope you are well, my name "+encodeURIComponent(inputName)+ " and my email is "+encodeURIComponent(inputEmail)+ ". I would like to book you for an Upcoming Event. ";
     window.open(formUrl, '_blank').focus();
 
 } 
